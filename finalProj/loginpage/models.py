650dcb032b8c63fe46ff36a822ca409c4c715902
Devyn Smith <devyn.smith17@gmail.com>
2020-11-18 14:35:26 -0700

    Co-authored-by: Ezra Frost <ezrabf@byu.edu> Co-authored-by: Stephan Peck <stephanpeck@msn.com>

--- finalProj/loginpage/models.py
+++ finalProj/loginpage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=10)
+
+    def __str__(self) :
+        return (self.user_name)
+
+    class Meta:
+        db_table = 'users'
--- finalProj/viewgroups/models.py
+++ finalProj/viewgroups/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+
+    group_name = models.CharField(maxlength=30)
+    group_description = models.CharField(maxlength=100)
+    classID = models.ForeignKey(viewclasses, on_delete=models.DO_NOTHING)
+
+    def __str__(self) :
+        return (self.group_name)
+
+    class Meta:
+        db_table = 'groups'
+
+class GroupMember(models.Model):
+    is_owner = models.BooleanField()
+    groupID = models.ForeignKey(viewgroups, on_delete=model.DO_NOTHING)
+    personID = models.ForeignKey(loginpage, on_delete=model.DO_NOTHING)
+
+    def __str__(self) :
+        return (self.personID)
+
+    class Meta:
+        db_table = 'group_members'
