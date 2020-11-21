1d78a44145564614455be8edd8ef8b3f99b6c05a
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2020-11-20 21:07:22 -0700

    we have everything working so that it uploads and correctly makes models on postgres

--- finalProj/finalProj/settings.py
+++ finalProj/finalProj/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'finalProj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'learningSweet',
+        'NAME': 'learningsweet',
         'USER': 'postgres',
         'PASSWORD': getpass(),
         'HOST': 'localhost'
