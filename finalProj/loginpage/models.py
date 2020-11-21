c3deb8edf333f4b7316576cf56f001a7e73419ee
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2020-11-20 20:30:01 -0700

    put all of the models into one page, login.models.py

--- finalProj/loginpage/models.py
+++ finalProj/loginpage/models.py
@@ -3,15 +3,50 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     user_name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-    email = models.EmailField(max_length=50)
+    email = models.CharField(max_length=50)
     phone = models.CharField(max_length=10)
 
-    def __str__(self) :
+    def __str__(self):
         return (self.user_name)
-
+    
     class Meta:
         db_table = 'users'
+
+class Classes(models.Model):
+
+    class_description = models.CharField(max_length=100)
+
+    def __str__(self) :
+        return (self.class_description)
+
+    class Meta :
+        db_table = 'classes'
+
+class Group(models.Model) :
+
+    group_name = models.CharField(max_length=30)
+    group_description = models.CharField(max_length=100)
+    classes = models.ForeignKey(Classes, on_delete=models.PROTECT)
+
+    def __str__(self) :
+        return (self.group_name)
+
+    class Meta :
+        db_table = 'groups'
+
+class GroupMember(models.Model):
+
+    is_owner = models.BooleanField()
+    group = models.ForeignKey(Group, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self) :
+        return (self.user)
+
+    class Meta :
+        db_table = 'group_members'
--- finalProj/viewgroups/models.py
+++ finalProj/viewgroups/models.py
@@ -1,30 +1,3 @@
 from django.db import models
-from django.apps import apps
 
-#importing models from other apps
-Classes = apps.get_model('viewclasses', 'Classes')
-User = apps.get_model('loginpage', 'User')
-
-# Create your models here.
-class Group(models.Model):
-
-    group_name = models.CharField(maxlength=30)
-    group_description = models.CharField(maxlength=100)
-    classes = models.ForeignKey(Classes, on_delete=models.PROTECT)
-
-    def __str__(self) :
-        return (self.group_name)
-
-    class Meta:
-        db_table = 'groups'
-
-class GroupMember(models.Model):
-    is_owner = models.BooleanField()
-    group = models.ForeignKey(Group, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-
-    def __str__(self) :
-        return (self.user)
-
-    class Meta:
-        db_table = 'group_members'
+#Create your models here.
