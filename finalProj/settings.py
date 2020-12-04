77333fda864542d2727417b4f5286c35e57b9c22
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2020-12-04 11:32:07 -0700

    y

--- finalProj/settings.py
+++ finalProj/settings.py
@@ -160,8 +160,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-DISABLE_COLLECTSTATIC=1
-
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
